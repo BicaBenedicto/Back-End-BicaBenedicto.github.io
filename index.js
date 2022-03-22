6b931e29689f38e0b20b328d93381af764572e48
Gabriel <gabrielpbenedicto@gmail.com>
2022-03-22 01:13:55 -0300

    feat: adiciona a rota login

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const technologiesRouter = require('./routes/technologies.routes');
 const projectsRouter = require('./routes/projects.routes');
+const loginRouter = require('./routes/login.routes');
 const errorsMiddleware = require('./errors');
 
 const app = express();
@@ -9,6 +10,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use('/login', loginRouter);
 app.use('/technologies', technologiesRouter);
 app.use('/projects', projectsRouter);
 
