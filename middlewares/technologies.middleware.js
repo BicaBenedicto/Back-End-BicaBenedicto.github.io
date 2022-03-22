882b16b8a3760cb2579b8c5448b08136443735d1
Gabriel <gabrielpbenedicto@gmail.com>
2022-03-22 09:17:11 -0300

    feat: comenta jwt verificação

--- middlewares/technologies.middleware.js
+++ middlewares/technologies.middleware.js
@@ -7,12 +7,12 @@ const get = async (_require, _response, next) => next();
 const remove = async (require, _response, next) => {
   const { id } = require.params;
 
-  const token = require.headers.authorization;
-  if (!token) return next('tokenEmpty');
+  // const token = require.headers.authorization;
+  // if (!token) return next('tokenEmpty');
 
-  const validateToken = await TOKEN(token);
+  // const validateToken = await TOKEN(token);
 
-  if (!validateToken) return next('tokenInvalid');
+  // if (!validateToken) return next('tokenInvalid');
 
   const validate = await removeValidation.validate({ id });
   if(validate.error) return next(validate.error);
@@ -26,12 +26,12 @@ const remove = async (require, _response, next) => {
 const create = async (require, _response, next) => {
   const { body } = require;
 
-  const token = require.headers.authorization;
-  if (!token) return next('tokenEmpty');
+  // const token = require.headers.authorization;
+  // if (!token) return next('tokenEmpty');
 
-  const validateToken = await TOKEN(token);
+  // const validateToken = await TOKEN(token);
 
-  if (!validateToken) return next('tokenInvalid');
+  // if (!validateToken) return next('tokenInvalid');
 
   const validate = await createValidation.validate(body);
   if(validate.error) return next(validate.error);
@@ -43,12 +43,12 @@ const update = async (require, _response, next) => {
   const { body } = require;
   const { id } = require.params;
 
-  const token = require.headers.authorization;
-  if (!token) return next('tokenEmpty');
+  // const token = require.headers.authorization;
+  // if (!token) return next('tokenEmpty');
 
-  const validateToken = await TOKEN(token);
+  // const validateToken = await TOKEN(token);
 
-  if (!validateToken) return next('tokenInvalid');
+  // if (!validateToken) return next('tokenInvalid');
 
   const validate = await updateValidation.validate({...body, id});
   if(validate.error) return next(validate.error);
