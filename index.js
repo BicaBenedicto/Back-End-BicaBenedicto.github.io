a69c982aa051d54c6b8450610d0b97097c3518fb
Gabriel <gabrielpbenedicto@gmail.com>
2022-03-22 08:22:54 -0300

    feat: restaura configurações iniciais

--- index.js
+++ index.js
@@ -4,18 +4,10 @@ const technologiesRouter = require('./routes/technologies.routes');
 const projectsRouter = require('./routes/projects.routes');
 const loginRouter = require('./routes/login.routes');
 const errorsMiddleware = require('./errors');
-const cors = require('cors');
-
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    methods: 'GET, PUT, POST, DELETE',
-    optionsSuccessStatus: 200,
-};
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(express.json());
 
 app.use('/login', cors(corsOptions), loginRouter);
